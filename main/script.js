26391395bb80b010fdec8ac4d46e87b811c212ca
Alex Monteiro <alexxmontteiro@gmail.com>
2026-03-27 18:10:52 -0300

    style: animated catalog cards

--- main/script.js
+++ main/script.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function openGoh(){
     const mod = document.getElementById('modGoh'); /* Gets The God of High School modal */
     mod.classList.add('open') /* Shows modal */
-    
+
     mod.addEventListener('click', (e) =>{
         if(e.target.id == 'close'){ /* Checks if clicked element is the close button */
             mod.classList.remove('open'); /* Hides modal */
